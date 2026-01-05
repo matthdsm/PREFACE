c496185f55d9ffcea2a11d08d569e48590f8aede
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2026-01-05 15:34:46 +0100

    add test results

---
+++ test_data/test_all.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+for IMPUTE_TYPE in zero mean median knn; do
+#for IMPUTE_TYPE in zero mice mean median knn; do
+    for MODEL_TYPE in neural xgboost svm; do
+        echo "--- Running ${MODEL_TYPE} with ${IMPUTE_TYPE} ---"
+        pixi run PREFACE train \
+            --samplesheet samplesheet.tsv \
+            --outdir ./output/${MODEL_TYPE}_${IMPUTE_TYPE} \
+            --model ${MODEL_TYPE} \
+            --impute ${IMPUTE_TYPE}
+    done
+done
